6da4490f7179f9167cd4ea4dd144ac27658c725e
Abdulkadir <qansax30@hotmail.com>
2024-02-06 00:49:20 +0100

    it is also done this project !

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from 'react'
+
+function Footer() {
+  const [year, setYear] = useState();
+
+  useEffect(() => {
+    const date = new Date();
+    setYear(date.getFullYear());
+  }, [year]);
+
+  return (
+    <footer className='w-full bg-primaryColor'>
+      <div className='container p-12 flex flex-col items-center justify-center'>
+        <h2 className='font-medium text-2xl text-white text-center'>Real Estate  </h2>
+        <p className='text-xs sm:text-sm text-center mt-9 pb-0 text-white'> © {year} Abdulkadir Mohamud - All rigths reserved</p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Hero from "../../components/homeContent/Hero";
+import LatestProperty from "../../components/homeContent/LatestProperty";
+import Features from "../../components/homeContent/Features";
+
+
+
+function Home() {
+  return (
+    <div className="w-full">
+      <Hero />
+      <Features/>
+      <LatestProperty />
+    </div>
+  );
+}
+
+export default Home;
